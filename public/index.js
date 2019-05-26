d355285531a2595ae19e953e1fd6d1637d4271a4
Hannah Pretswell <hannimator@gmail.com>
2019-05-26 16:29:42 +0100

    added jquery so item 0 from db can be shown on page

--- public/index.js
+++ public/index.js
@@ -2,6 +2,18 @@ const selectElement = document.querySelector('.type');
 
 selectElement.addEventListener('change', (event) => {
     const httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET', 'http://localhost:5000/moves?type=' + event.target.value, true);
+    httpRequest.open('GET', `http://localhost:5000/moves?type=${event.target.value}`, false);
     httpRequest.send();
+
+    const moves = JSON.parse(httpRequest.response);
+
+    const paragraph = $('<p></p>');
+    paragraph.append(`Difficulty: ${moves[0].difficulty}`)
+        .append('<br />')
+        .append(`Type: ${moves[0].type}`);
+
+    $('#moves').append('<div>')
+        .append(`<h2> ${moves[0].name} </h2>`)
+        .append(paragraph)
+        .append('</div>');
 });
